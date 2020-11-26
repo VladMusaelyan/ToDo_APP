35769ae3fd9e407a5a6aa54dca4b40d77d95c708
Vlad <musaelyanvlad@gmail.com>
2020-11-26 14:44:54 +0400

    removing some tasks and disabled buutons

--- src/toDo/Task.js
+++ src/toDo/Task.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import styles from './TaskStyles.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
+import { Button, Row, Col, Card } from 'react-bootstrap';
 
-export function Task(props) {
-    return (
-        <>
-            { props.text}
-        </>
-    );
+class Task extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            element: this.props.element
+        }
+    }
+    render() {
+        const { element } = this.state
+        return (
+            <>
+                <Card className={element.checked === true && 'border border-danger'}>
+                    <Card.Body>
+                        <Row>
+                            <Col xs={10} sm={10} md={10} lg={10} xl={10}>
+                                <Card.Title>
+                                    {element.text.slice(0, 10) + '...'}
+                                </Card.Title>
+                            </Col>
+                            <Col xs={2} sm={2} md={2} lg={2} xl={2}>
+                                <input
+                                    type="checkbox"
+                                    className={styles.checkbox}
+                                    onClick={() => this.props.checkboxClick(element._id)}
+                                />
+                            </Col>
+                        </Row>
+                        <Card.Text>
+                            {element.text}
+                        </Card.Text>
+                        <div className='d-flex flex-row-reverse'>
+                            <Button
+                                variant="warning"
+                                disabled={this.props.disabled}
+                                className='ml-3'
+                            >
+                                <FontAwesomeIcon icon={faEdit} />
+                            </Button>
+                            <Button
+                                variant="danger"
+                                onClick={() => this.props.onRemoveTask(element._id)}
+                                disabled={this.props.disabled}
+                            >
+                                <FontAwesomeIcon icon={faTrash} />
+                            </Button>
+                        </div>
+                    </Card.Body>
+                </Card>
+            </>
+        );
+    }
 }
 
+export default Task;
--- src/toDo/ToDo.js
+++ src/toDo/ToDo.js
@@ -2,17 +2,17 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable eqeqeq */
 import React from 'react';
-import styles from './taskStyles.module.css';
-import { Task } from './Task';
+import Task from './Task';
 import IdGenerator from '../assets/IDGenerator';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash, faEdit } from '@fortawesome/free-solid-svg-icons';
-import { Button, InputGroup, FormControl, Row, Col, Container, Card } from 'react-bootstrap';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+import { Button, InputGroup, FormControl, Row, Col, Container } from 'react-bootstrap';
 
 class ToDo extends React.Component {
     state = {
         tasks: [],
         inputValue: '',
+        boolean: 0                              //boolean for disabled buutons
     }
     handleChange = (event) => {
         this.setState({
@@ -24,11 +24,13 @@ class ToDo extends React.Component {
         this.setState({
             tasks: [{
                 _id: IdGenerator(),
-                text: inputValue
+                text: inputValue,
+                checked: false
             }, ...tasks],
             inputValue: ''
         })
     }
+    // removing current task
     removeTask = (id) => {
         const removeTask = this.state.tasks.filter(element => element._id !== id);
         this.setState({
@@ -39,69 +41,88 @@ class ToDo extends React.Component {
         e.key === 'Enter' && this.setState({
             tasks: [{
                 _id: IdGenerator(),
-                text: this.state.inputValue
+                text: this.state.inputValue,
+                checked: false
             }, ...this.state.tasks],
             inputValue: ''
+        })
+    }
+    checkboxClick = (id) => {
+        const { tasks, boolean } = this.state;
+        tasks.forEach(element => {
+            if (element._id === id) {
+                element.checked = !element.checked;
+                if (element.checked === true) {
+                    this.setState({
+                        boolean: boolean + 1
+                    })
+                } else {
+                    this.setState({
+                        boolean: boolean - 1
+                    })
+                }
+            }
+        })
+        this.setState({
+            inputValue: ''
+        })
+    }
+    // removing some tasks
+    removeTasks = () => {
+        const removeTasks = this.state.tasks.filter(element => element.checked !== true);
+        this.setState({
+            tasks: removeTasks,
+            boolean: 0
         })
     }
     render() {
         const tasks = this.state.tasks.map((element) => {
             return (
-                <Col className='mt-3' key={element._id} xs={12} sm={12} md={6} lg={4} xl={3}>
-                    <Card>
-                        <Card.Body>
-                            <Card.Title>
-                                {element.text.slice(0, 10)}
-                                {element.text !== '' ? element.text.length > 10 && '...' : 'Empty Task'}</Card.Title>
-                            <Card.Text>
-                                {element.text}
-                            </Card.Text>
-                            <div className='d-flex flex-row-reverse'>
-                                <Button
-                                    variant="warning"
-                                    className='ml-3'
-                                >
-                                    <FontAwesomeIcon icon={faEdit} />
-                                </Button>
-                                <Button
-                                    variant="danger"
-                                    onClick={() => this.removeTask(element._id)}
-                                >
-                                    <FontAwesomeIcon icon={faTrash} />
-                                </Button>
-                            </div>
-                        </Card.Body>
-                    </Card>
+                <Col className='mt-3' key={element._id} xs={12} sm={12} md={6} lg={4} xl={4}>
+                    <Task
+                        element={element}
+                        onRemoveTask={this.removeTask}
+                        checkboxClick={this.checkboxClick}
+                        disabled={!!this.state.boolean}
+                    />
                 </Col>
             )
         });
         return (
             <div >
                 <Container>
-                    <Row className={styles.container + ' p-3'}>
+                    <Row className={'p-3 d-flex justify-content-center'}>
                         <Col xs={12} sm={12} md={12} lg={10} xl={10}>
                             <InputGroup className='mb-3'>
                                 <FormControl
                                     placeholder="Add a new task"
                                     aria-describedby="basic-addon2"
+                                    disabled={this.state.boolean}
                                     value={this.state.inputValue}
                                     onChange={this.handleChange}
                                     onKeyDown={this.handleKeyDown}
-                                    className={styles.input}
                                 />
                                 <InputGroup.Append>
                                     <Button
                                         variant="outline-primary"
                                         onClick={this.handleClick}
+                                        disabled={this.state.inputValue === ''}
                                     >
                                         Add
+                                    </Button>
+                                    <Button
+                                        variant="outline-danger"
+                                        disabled={!this.state.boolean}
+                                        onClick={this.removeTasks}
+                                    >
+                                        <FontAwesomeIcon icon={faTrash} />
                                     </Button>
                                 </InputGroup.Append>
                             </InputGroup>
                         </Col>
                     </Row>
                     <Row>
-                        <Task text={tasks} />
+                        {tasks}
                     </Row>
                 </Container>
             </div >
